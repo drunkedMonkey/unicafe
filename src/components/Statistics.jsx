5cdef4dfd320b8e1b7d1dbba8c540272a8334b16
Jose Ibañez <appibanez@gmail.com>
2024-10-29 19:45:59 +0100

    created component statisticline to do statistic independient

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,3 +1,5 @@
+import StatisticLine from "./StatisticLine";
+
 // eslint-disable-next-line react/prop-types
 const Statistics = ({ good, neutral, bad }) => {
   if (good === 0 && neutral === 0 && bad === 0) {
@@ -8,7 +10,7 @@ const Statistics = ({ good, neutral, bad }) => {
       </>
     );
   }
-  
+
   const all = good + neutral + bad;
   const average = (good - bad) / all;
   const positive = (good / all) * 100;
@@ -16,12 +18,12 @@ const Statistics = ({ good, neutral, bad }) => {
     <>
       <h1>Statistics</h1>
       <div className="statistics">
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive} />
       </div>
     </>
   );
